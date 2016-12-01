656838f2cd9e8246a6ed6c7b147763200638b35e
john <cdzhangw@gmail.com>
2016-12-01 14:25:44 +0800

    change to js syntax

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 var Lawnchair =require('lawnchair');
 module.exports.LawnchairPromise=function(options) {
-	return new Promise((resolve, reject)=>{
-		new Lawnchair(options, function(store) {
-			const ret = {};
-			['keys','save','batch','get','exists','each','all','remove','nuke'].forEach(key=>{
-				ret[key] = function() {
-					var args = Array.prototype.slice.apply(arguments);
-					return new Promise(resolve1=>{
-						args.push(resolve1);
-						store[key].apply(store, args);
-					});
-				}
-			});
-			resolve(ret);
-		});
-	});
+        var ret = {};
+        ['keys','save','batch','get','exists','each','all','remove','nuke'].forEach(function(key){
+                ret[key] = function() {
+                        var args = Array.prototype.slice.apply(arguments);
+                        return new Promise(function(resolve1,reject1){
+                                args.push(resolve1);
+                                new Lawnchair(options, function(store) {
+                                        store[key].apply(store, args);
+                                })
+                        });
+                }
+        });
+		return ret;
 
 }
